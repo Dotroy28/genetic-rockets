ac88f866a108a1afb63797d22e9901927fe6c12e
Michael Hum <michaelhum28@gmail.com>
2023-10-29 11:33:57 -0400

    feat fixed errors and allowed for loop

--- genetic-rockets.js
+++ genetic-rockets.js
@@ -1,25 +1,138 @@
-var rocket;
+var population;
+var lifespan =200;
+var lifeP;
+var count= 0;
+var target;
 
 function setup(){
     createCanvas(400,300);
     rocket = new Rocket();
-}
-function draw(){
-    background(0);
-    rocket.update();
-    rocket.show();
+    population = new Population();
+    lifeP = createP();
+    target = createVector(width/2,50);
 }
 
-function Rocket(){
+function draw(){
+    background(0);
+    population.run();
+    lifeP.html(count);
+
+    count++;
+    if (count == lifespan) {
+        population.evaluate();
+        population.selection();
+        count = 0; // Reset count to restart the simulation
+    }
+
+
+    ellipse(target.x,target.y,16,16);
+}
+
+function Population(){
+    this.rockets=[];
+    this.popsize=25;
+    this.matingpool=[];
+
+    for(var i = 0;i<this.popsize;i++){
+        this.rockets[i] = new Rocket();
+    }
+
+    this.evaluate = function(){
+
+        var maxfit=0;
+        for(var i =0;i<this.popsize;i++){
+            this.rockets[i].calcFitness();
+            if(this.rockets[i].fitness>maxfit){
+                maxfit= this.rockets[i].fitness;
+            }
+        }
+
+        for(var i =0;i<this.popsize;i++){
+            this.rockets[i].fitness /= maxfit; //normalize it
+        }
+
+        this.matingpool=[];
+        for(var i =0;i<this.popsize;i++){
+            var n = this.rockets[i].fitness * 100;
+            for(var j = 0;j < n;j++){
+                this.matingpool.push(this.rockets[i]);
+            }
+        }
+    }
+        
+
+        this.selection = function(){
+            var newRockets=[];
+            for(var i = 0;i<this.rockets.length;i++){
+            var parentA = random(this.matingpool).dna; //give random element from the array (feature in p5?)
+            var parentB = random(this.matingpool).dna;
+            var child = parentA.crossover(parentB);
+            newRockets[i]=new Rocket(child);
+            }
+            this.rockets = newRockets;
+        }
+
+    this.run = function(){
+        for(var i = 0;i<this.popsize;i++){
+            this.rockets[i].update();
+            this.rockets[i].show();
+        }
+    }
+}
+
+function DNA(genes){
+    if(genes){
+        this.genes=genes;
+    }else{
+        this.genes=[];
+        for(var i = 0;i<lifespan;i++){
+            this.genes[i] = p5.Vector.random2D();
+            this.genes[i].setMag(0.1);
+        }
+    }
+
+    this.crossover = function(partner){ //method - pick midpoint in length of the array and get half genes/genes
+        var newgenes = new DNA(); //make random genes
+        var mid = floor(random(this.genes.length));
+        for(var i = 0; i<this.genes.length;i++){
+            if(i>0){
+                newgenes[i] = this.genes[i];
+            } else{
+                newgenes[i] = partner.genes[i]; //overwrite
+            }
+            
+        }
+        return new DNA(newgenes); //return new dna with new genes
+    }
+}
+
+function Rocket(dna){
     this.pos = createVector(width/2, height);
-    this.vel = createVector(0,-1);
+    this.vel = createVector();
     this.acc = createVector();
+
+    if(dna){
+        this.dna=dna;
+    }else{
+        this.dna = new DNA();
+    }
+    this.fitness = 0;
 
     this.applyForce = function(force){
         this.acc.add(force);
     }
 
+    this.calcFitness = function(){
+        var d = dist(this.pos.x, this.pos.y, target.x, target.y);
+
+        this.fitness=map(d,0,width,width,0);
+    }
+
     this.update = function(){
+        this.applyForce(this.dna.genes[count]);
+
+
+
         this.vel.add(this.acc);
         this.pos.add(this.vel);
         this.acc.mult(0);
@@ -27,10 +140,12 @@ function Rocket(){
 
     this.show = function(){
         push();
+        noStroke();
+        fill(255,150);
         translate(this.pos.x, this.pos.y);
         rotate(this.vel.heading());
         rectMode(CENTER);
-        rect(0,0,10,50);
+        rect(0,0,25,5);
         pop();
     }
 }
