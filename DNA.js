f5e1a69b6cf7296cb71d8fc2b54bf75e22c7b71f
Michael Hum <michaelhum28@gmail.com>
2023-11-02 21:08:21 -0400

    fix: genes updating based on fitness level and not randomizing

--- DNA.js
+++ DNA.js
@@ -1,36 +1,34 @@
-function DNA(genes){
-  if(genes){
-      this.genes=genes;
-  }else{
-      this.genes=[];
-  }
-  for(var i = 0;i<lifespan;i++){
-    this.genes[i] = p5.Vector.random2D();
-    this.genes[i].setMag(maxforce);
-  }
-
-
-  this.crossover = function(partner){ //method - pick midpoint in length of the array and get half genes/genes
-      var newgenes = new DNA(); //make random genes
-      var mid = floor(random(this.genes.length));
-      for(var i = 0; i<this.genes.length;i++){
-          if(i>mid){
-              newgenes[i] = this.genes[i];
-          } else{
-              newgenes[i] = partner.genes[i]; //overwrite
-          }
-          
-      }
-      return new DNA(newgenes); //return new dna with new genes
-  }
-
-  this.mutation = function(){
-    for(var i = 0;i<genes.length;i++){
-        if(random(1)<0.01){
-            this.genes[i]=p5.Vector.random2D();
+function DNA(genes) {
+    if(genes) {
+        this.genes = genes;
+    } else {
+        this.genes = [];
+        for(var i = 0; i < lifespan; i++) {
+            this.genes[i] = p5.Vector.random2D();
             this.genes[i].setMag(maxforce);
         }
     }
-  }
+
+    this.crossover = function(partner) { //method - pick midpoint in length of the array and get half genes/genes
+        var newgenes = [];
+        var mid = floor(random(this.genes.length));
+        for(var i = 0; i < this.genes.length; i++) {
+            if(i > mid) {
+                newgenes[i] = this.genes[i];
+            } else {
+                newgenes[i] = partner.genes[i];
+            }
+        }
+        return new DNA(newgenes);
+    }
+
+    this.mutation = function() {
+        for(var i = 0; i < this.genes.length; i++) {
+            if(random(1) < 0.01) {
+                this.genes[i] = p5.Vector.random2D();
+                this.genes[i].setMag(maxforce);
+            }
+        }
+    }
 
 }
